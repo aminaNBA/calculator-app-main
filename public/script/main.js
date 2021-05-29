fa7bd9cff59de7d3838c1d06459db94d9d644ea4
Amina Madat <aminanba9@gmail.com>
2021-05-29 15:26:11 +0200

    organization files

---
+++ public/script/main.js
@@ -0,0 +1,22 @@
+let seven = document.getElementById('seven')
+let eight = document.getElementById('eight')
+let nine = document.getElementById('nine')
+let del = document.getElementById('del')
+let four = document.getElementById('four')
+let five = document.getElementById('five')
+let six = document.getElementById('six')
+let plus = document.getElementById('plus')
+let one = document.getElementById('one')
+let two = document.getElementById('two')
+let three = document.getElementById('three')
+let minus = document.getElementById('minus')
+let dot = document.getElementById('dot')
+let zero = document.getElementById('zero')
+let devise = document.getElementById('devise')
+let multiple = document.getElementById('multiple')
+let reset = document.getElementById('reset')
+let equal = document.getElementById('equal')
+
+function numbers() {
+    console.log('hello')
+  }
